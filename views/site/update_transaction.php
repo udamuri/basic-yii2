948e1ebfeb3fbe435fa5407a1611294a271e493f
Muri Budiman <udamuri@gmail.com>
2018-03-24 12:44:07 +0700

    fixing crud transactions

---
+++ views/site/update_transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+//use yii\helpers\ArrayHelper;
+
+$this->title = 'Tambah';
+$this->params['breadcrumbs'][] = [
+    'label' =>'Transaksi',
+    'url' => Yii::$app->homeUrl.'site/transaksi'
+];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <?= $this->render('_form_transaction', [
+            'model' => $model,
+            'form_id' => 'form-create-transaction',
+            'button' => 'Simpan',
+        ]) ?>
+    </div>
+</div>
